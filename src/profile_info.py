a9605ee0e75a8a83a6c0d1c47aa3468b7f3685bd
Rona Wang <rona@mit.edu>
2023-10-05 08:57:52 -0400

    set up api endpoint

--- src/profile_info.py
+++ src/profile_info.py
@@ -1,3 +1,6 @@
+import os
+from pyairtable import Api
+
 PROFILES = {
     "first_name": "John",
     "last_name": "Smith",
@@ -11,16 +14,22 @@ PROFILES = {
     "github": "https://github.com/ChadLei/",
     "twitter": "",
     "location": "San Francisco, California, United States",
-    "grad_month": '06',
-    "grad_year": '2024',
+    "grad_month": "06",
+    "grad_year": "2024",
     "university": "University, USA",
     "most_recent_employer": "University",
-    "reason_of_interest": "Having honed my skills in various software projects, I believe in the power of code to drive transformative change, and I am always eager to contribute to environments that champion creativity and problem-solving."
+    "reason_of_interest": "Having honed my skills in various software projects, I believe in the power of code to drive transformative change, and I am always eager to contribute to environments that champion creativity and problem-solving.",
 }
+
+api = Api(
+    os.environ[
+        "patGVfkDXNA8c54m9.2cbd2d736cdfcfa3c2c73aee4ed6761f2085916f908d3f803213cdcf58a62a7e"
+    ]
+)
+table = api.table("appxBfSaIMlRGrhBZ", "tbltJNGPYAGz8IIOh")  # base ID, table ID
+
 
 class Profile:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-
-
